f795ceb7c6814cd4d7bf880906d477f6d2ff0ce4
eugene-masuda <eugene0531masuda@yahoo.co.jp>
2020-05-21 05:11:14 +0000

    Finish user edit, update, index, and destroy actions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_21_010858) do
+ActiveRecord::Schema.define(version: 2020_05_21_045202) do
 
   create_table "users", force: :cascade do |t|
     t.string "full_name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2020_05_21_010858) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
   end
 
 end
