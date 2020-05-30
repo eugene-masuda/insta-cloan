c5553872921504f374a4bb0983a4a5f9e24810c8
eugene-masuda <eugene0531masuda@yahoo.co.jp>
2020-05-30 01:48:22 +0000

    search

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,6 +4,12 @@ class StaticPagesController < ApplicationController
       @user = current_user
       @micropost  = current_user.microposts.build
       @feed_items = current_user.feed.paginate(page: params[:page])
+      if params[:q]
+        @feed_items = Micropost.search_by_keyword(params[:q])
+                        .paginate(page: params[:page])
+      else
+        @feed_items = current_user.feed.paginate(page: params[:page])
+      end
     end
   end
 
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,6 +4,9 @@ class Micropost < ApplicationRecord
   has_many :favorite_relationships, dependent: :destroy
   has_many :liked_by, through: :favorite_relationships, source: :user
   default_scope -> { order(created_at: :desc) }
+  scope :search_by_keyword, -> (keyword) {
+    where("microposts.content LIKE :keyword", keyword: "%#{sanitize_sql_like(keyword)}%") if keyword.present?
+  }
   has_many :comments, dependent: :destroy
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
